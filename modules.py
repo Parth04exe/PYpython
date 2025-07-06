f36b4a1d3729d5c903c55ed9d7d0574bca2b732d
Parth04exe <mohantyparthasarathy04@gmail.com>
2025-07-06 23:10:59 +0530

    LEARNING

---
+++ modules.py
@@ -0,0 +1,14 @@
+import pyjokes
+
+print("printing jokes....")
+
+# this prints a random joke single line comment"single line comments"
+
+joke= pyjokes.get_joke()
+print(joke)
+"""
+thanks that was my program "multi line comments"
+"""
+'''
+one more way
+'''
