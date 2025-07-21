7d8005e992851f35a7bd4bdbc750fd981ed947d0
Parth04exe <mohantyparthasarathy04@gmail.com>
2025-07-21 09:04:17 +0530

    learn

---
+++ py6/01dict.py
@@ -0,0 +1,18 @@
+marks = {
+    "Harry": 100,
+    "Shubham": 56,
+    "Rohan": 23
+}
+
+print(marks,type(marks))
+print(marks["Harry"])
+
+# marks2=[["Harry", 100],["Parth", 100],["Sammy", 100]]
+# print(marks2[0])
+
+a={
+    "key":"value",
+    "Harry":"code",
+    "lst":[1,2,9]
+}
+print(a["lst"])
