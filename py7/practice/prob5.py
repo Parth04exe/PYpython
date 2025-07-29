142c7b9457bdf19670d2ab306ae28ef68a13efc9
Parth04exe <mohantyparthasarathy04@gmail.com>
2025-07-29 09:58:59 +0530

    learn

---
+++ py7/practice/prob5.py
@@ -0,0 +1,7 @@
+l=["Harry","Rohan","Shubham","Divya"]
+name=input("Enter your name: ")
+
+if (name in l):
+    print("name is in the list")
+else:
+    print("name is not in the list")
---
+++ py7/practice/prob7.py
@@ -0,0 +1,5 @@
+post=input("enter your post: ")
+if("Harry".lower() in post.lower()):# all case
+    print("This post is talkng about harry")
+else:
+    print("this post is not talking about harry")
