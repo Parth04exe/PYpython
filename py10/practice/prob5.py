1b7254934f56c758dc2b4d1017e0c39147d8cdb6
Parth04exe <mohantyparthasarathy04@gmail.com>
2025-08-07 15:19:10 +0530

    learn

---
+++ py10/practice/prob5.py
@@ -0,0 +1,7 @@
+words = ["Donkey", "bad", "ganda"]
+with open("file2.txt", "r") as f:
+    content = f.read()
+for word in words:
+    content = content.replace(word,"#"*len(word))
+with open("file2.txt","w") as f:
+    f.write(content)
